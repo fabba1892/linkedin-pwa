5996f6190d2b01e36ac3e4d85319df1f1b8889d1
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2021-03-08 15:56:19 +0200

    login, logout do the selec & pul frm da dataLayer

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -21,3 +21,5 @@ export const { login, logout } = userSlice.actions;
 export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
+// login, logout do the select pul frm da dataLayer
+
